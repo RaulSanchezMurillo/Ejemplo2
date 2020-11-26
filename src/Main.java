f417f1b72a3a6526bf73280a990804c97153bb5e
RaulSanchezMurillo <prsangal@iesmurillo.es>
2020-11-26 01:11:37 +0100

    Añadido metodo para sumar

--- src/Main.java
+++ src/Main.java
@@ -2,7 +2,15 @@
 public class Main {
 
 	public static void main(String[] args) {
-		System.out.println("d Pepe" );
+		int resultado=suma(8,9);
+		System.out.println("d Pepe"+resultado);
 	}
+	
+	
+	public static int suma(int n1, int n2) {
+		return n1+n2;
+	}
+	
+
 
 }
