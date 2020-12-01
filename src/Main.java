f87f077edfbb936402c48be0e4caa5d457311ee5
RaulSanchezMurillo <prsangal@iesmurillo.es>
2020-12-01 12:20:57 +0100

    12

--- src/Main.java
+++ src/Main.java
@@ -3,7 +3,7 @@ public class Main {
 
 	public static void main(String[] args) {
 
-		int resultado=suma(10,11);
+		int resultado=suma(10,12);
 		System.out.println("d Pepenfvvbnn"+resultado);
 	}
 	
